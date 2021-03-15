aeafe9b4426fd5a77697040508d8b5fb9a6c8394
Buck Perley <bucko.perley@gmail.com>
2021-03-15 15:46:18 -0500

    add header

--- pages/_app.tsx
+++ pages/_app.tsx
@@ -1,9 +1,18 @@
 // top level component that wraps each page
 import "../styles/index.css"
+import Head from "next/head"
 
 function MyApp({ Component, pageProps }) {
   return (
     <>
+      <Head>
+        <title>Bitcoin Fee Alerts</title>
+        <meta property="og:title" content="Bitcoin Fee Alerts" key="title" />
+        <meta
+          name="description"
+          content="Signup for alerts about changing fee conditions on the bitcoin network"
+        />
+      </Head>
       <div className="font-mono">
         <Component {...pageProps} />
       </div>
